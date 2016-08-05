d8e609ed1ad8d3dfbe1c939339ecf5ea02806a39
imac <imac@OwendeiMac.local>
2016-08-05 16:23:20 +0800

    no message

---
+++ UISearchController简单使用/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UISearchController简单使用
+//
+//  Created by imac on 16/8/5.
+//  Copyright © 2016年 imac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UISearchController简单使用/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UISearchController简单使用
+//
+//  Created by imac on 16/8/5.
+//  Copyright © 2016年 imac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
